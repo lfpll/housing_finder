ff8cc3c444a3614a3f215deab2fbc1955bb558f3
lfpll <G2l4u8i*6>
2019-06-03 21:22:21 -0300

    adding updates to child and father

--- child/parse_child_page.py
+++ child/parse_child_page.py
@@ -3,29 +3,45 @@ from google.cloud import pubsub_v1
 from bs4 import BeautifulSoup
 import requests
 import base64
+import hashlib
 
+
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0'}
 
 def save_to_storage(data, context):
-    publisher = pubsub_v1.PublisherClient()
+	"""Receives an url on the data.data value
+	   Publish value to pub\sub if fails
+	   Download the page to google storage if it works
 
-    if 'data' in data:
-        name = base64.b64decode(data['data']).decode('utf-8')
-    url = name
-    response = requests.get(url)
-    # If the request fail send again to pubsub
-    products_topic = 'projects/educare-226818/topics/child_scrape'
-    if response.status_code != 200:
-        publisher.publish(products_topic, url.encode('utf-8'))
-    else:
-	    # Get the ifle name
-	    data_name = url.split('/')[-1]
-	    storage_client = storage.Client(project='educare')
-	    soup = BeautifulSoup(response.text)
-	    if soup.select('title')[0].text == 'Error 500':
-	    	publisher.publish(products_topic, url.encode('utf-8'))
-	    else:
-		    # Save to the blob
-		    bucket_name = 'imoveis_data'
-		    bucket = storage_client.get_bucket(bucket_name)
-		    blob = bucket.blob(data_name)
-		    blob.upload_from_string(response.content)
+	Arguments:
+			data {[type]} -- [description]
+			context {[type]} -- [description]
+	"""
+
+	# Getting the information from the page
+	name = base64.b64decode(data['data']).decode('utf-8')
+	url = name
+	response = requests.get(url,headers=headers)
+
+	# If the request fail send again to pubsub
+	products_topic = 'projects/educare-226818/topics/child_scrape'
+	publisher = pubsub_v1.PublisherClient()
+
+	# If the status is not 200 the requestor was blocked
+	if response.status_code != 200:
+		publisher.publish(products_topic, url.encode('utf-8'))
+	else:
+		# Get the filename name
+		storage_client = storage.Client(project='educare')
+		soup = BeautifulSoup(response.text)
+		if soup.select('title')[0].text == 'Error 500':
+			publisher.publish(products_topic, url.encode('utf-8'))
+		else:
+			# Saving the htl
+			m = hashlib.md5()
+			bucket_name = 'imoveis_data'
+			bucket = storage_client.get_bucket(bucket_name)
+			m.update(url)
+			hash_id = str(int(m.hexdigest(), 16))
+			blob = bucket.blob('/imoveis_web/{hash_id}'.format(hash_id=hash_id))
+			blob.upload_from_string(response.content)
---
+++ child/parse_html.py
@@ -0,0 +1,120 @@
+from bs4 import BeautifulSoup
+import re
+from google.cloud import error_reporting,storage
+import hashlib
+import json
+import unidecode
+regexp_price = re.compile('^(\w+)(?:\s*)R\$(?:\s*)(\w+\.?(?:\w+)?\,?(?:\w+)?)')
+regexp_markers = re.compile('markers=(.+?)\&')
+
+def parse_price(soup_price_block):
+	'''
+	Parse block price and return a list of tuples with each value
+	:param soup_price_block:
+	:return:
+	'''
+	if len(soup_price_block) != 1:
+		raise ValueError('The page has a different size on the price block')
+	price_list = soup_price_block[0].text.strip().split('\n')
+	price_list = [regexp_price.search(price) for price in price_list if regexp_price.search(price)]
+	price_list = [(price_regexp.group(1), float(price_regexp.group(2).replace('.', '').replace(',', '.'))) for
+	              price_regexp in price_list]
+	return price_list
+
+
+def parse_attributes(attrs_block):
+	'''
+	Parse attributes
+	:param attrs_block:
+	:return:
+	'''
+	if len(attrs_block) != 1:
+		raise ValueError('The page has a different size on the attributes block')
+	attrs_list = attrs_block[0].select('li')
+	attrs_list = [(attrs.find('span').text.strip(), unidecode.unidecode(attrs.find('b').text.strip())) for attrs in attrs_list]
+	return attrs_list
+
+
+def parse_additives(addts_block):
+	'''
+	Parse de additions of information in list format
+	:param addts_block:
+	:return:
+	'''
+	if len(addts_block) <= 1:
+		raise ValueError('The page has a different size on the aditions block')
+	audits_final_list = []
+	addits_list = [additives.find_all('li') for additives in addts_block]
+	[audits_final_list.extend(additive) for additive in addits_list]
+	audits_final_list = [unidecode.unidecode(auditive.text.strip()) for auditive in audits_final_list]
+	return audits_final_list
+
+
+def parse_get_location(local_block):
+	'''
+	Get the latitude and longitude values
+	:param local_block:
+	:return:
+	'''
+	if len(local_block) > 1:
+		raise ValueError('The page has a different size on the local block')
+	image_url = local_block[0].find('img')
+	url_parse = regexp_markers.search(image_url['src']).group(1).split(',')
+	lat_long = [float(float_val) for float_val in url_parse]
+	lat_long = [('latitude', lat_long[0]), ('longitute', lat_long[1])]
+	return lat_long
+
+
+def parse_page(html_data,html_path):
+	'''
+	Parse the html page from imoveis web
+	:param html_page: page of html
+	:return: a dict format value of parsed data
+	'''
+	try:
+		soup = BeautifulSoup(html_data,'lxml')
+
+		# Parsing the interesting data
+		price_block = soup.select('div.block-price-container')
+		attrs_block = soup.select('ul.section-icon-features')
+		addts_block = soup.select('ul.section-bullets')
+		local_block = soup.select('div.article-map')
+
+		# Transforming data indo a format of interest
+		price_tups = parse_price(price_block)
+		attrs_tup = parse_attributes(attrs_block)
+		addts_tup = parse_additives(addts_block)
+		local_tup = parse_get_location(local_block)
+
+		# Transforming into a final tup
+		final_tups = []
+		final_tups.extend(attrs_tup)
+		final_tups.append(('additions', addts_tup))
+		final_tups.extend(price_tups)
+		final_tups.extend(local_tup)
+		
+		json_file = json.dumps({unidecode.unidecode(key): val for key, val in final_tups})
+
+		m = hashlib.md5()
+		m.update(html_path.encode('utf-8'))
+		hex_name = str(int(m.hexdigest(), 16))
+		storage_client = storage.Client()
+		bucket = storage_client.get_bucket('imoveis_data')
+		blob = bucket.blob('json_files/{hex_name}.json'.format(hex_name=hex_name))
+
+		blob.upload_from_string(json_file)
+
+
+	except Exception as error:
+		print(error)
+		error_client = error_reporting.Client()
+		error_client.report_exception()
+
+def parse_all_files():
+	storage_client = storage.Client()
+	bucket = storage_client.get_bucket('imoveis_data')
+	blobs = bucket.list_blobs(prefix='imoveis_web/')
+	for blob in blobs:
+		path = blob.name.split('/')[-1]
+		parse_page(html_data=blob.download_as_string(),html_path=path)
+
