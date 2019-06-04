d0c91a7946e714b3659b73098379e87040d17900
lfpll <G2l4u8i*6>
2019-06-03 21:22:21 -0300

    remove unused files and add updates do father and child

--- father/parse_father_page.py
+++ father/parse_father_page.py
@@ -3,34 +3,43 @@ from google.cloud import pubsub_v1
 import requests
 import base64
 
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0'}
 
 def parse_head_page(data, context):
-    name = base64.b64decode(data['data']).decode('utf-8')
-    publisher = pubsub_v1.PublisherClient()
+	""" Parge the main page
+		Passing to the current pub\subs for each kind of page
 
-    url = name
-    response = requests.get(url)
 
-    pagination_topic = 'projects/educare-226818/topics/scrape'
-    # If the request fail send again to pubsub
-    if response.status_code != 200:
-        publisher.publish(pagination_topic, url.encode('utf-8'))
-    else:
-        # Next page
+	Arguments:
+		data {[type]} -- [description]
+		context {[type]} -- [description]
+	"""
+	name = base64.b64decode(data['data']).decode('utf-8')
+	publisher = pubsub_v1.PublisherClient()
 
-        father_url = "https://www.imovelweb.com.br"
-        soup = BeautifulSoup(response.content)
-        next_url = soup.select('li.pag-go-next')
-        if len(next_url) > 0:
-            next_url = next_url[0].select('a')[0]['href']
-            publisher.publish(pagination_topic, (father_url+next_url).encode('utf-8'))
+	url = name
+	response = requests.get(url,headers=headers)
 
-        products_topic = 'projects/educare-226818/topics/child_scrape'
-        # Pages of producst
-        products_soups = soup.select('a.go-to-posting')
-        products_url = [father_url+attribute['href']
-                        for attribute in products_soups]
+	pagination_topic = 'projects/educare-226818/topics/scrape'
+	# If the request fail send again to pubsub
+	if response.status_code != 200:
+		publisher.publish(pagination_topic, url.encode('utf-8'))
+	else:
+		# Next page
 
-        # Publishing urls to the products topic
-        for product in products_url:
-            publisher.publish(products_topic, product.encode('utf-8'))
+		father_url = "https://www.imovelweb.com.br"
+		soup = BeautifulSoup(response.content)
+		next_url = soup.select('li.pag-go-next')
+		if len(next_url) > 0:
+			next_url = next_url[0].select('a')[0]['href']
+			publisher.publish(pagination_topic, (father_url + next_url).encode('utf-8'))
+
+		products_topic = 'projects/educare-226818/topics/child_scrape'
+		# Pages of producst
+		products_soups = soup.select('a.go-to-posting')
+		products_url = [father_url + attribute['href']
+		                for attribute in products_soups]
+
+		# Publishing urls to the products topic
+		for product in products_url:
+			publisher.publish(products_topic, product.encode('utf-8'))
