f3cc90fcd7ac6ba9676c6d067d70c5161db35d81
Lobo <luizloureiro@alunos.utfpr.edu.br>
2019-07-05 21:53:48 -0300

    Better naming the files

--- child/parse_prop_html.py
+++ child/parse_prop_html.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup
 import re
 from google.cloud import storage,error_reporting
+from datetime import datetime
 import hashlib
 import json
 import base64
 import unidecode
+import requests
 
 regexp_price = re.compile('^(\w+)(?:\s*)R\$(?:\s*)(\w+\.?(?:\w+)?\,?(?:\w+)?)')
 regexp_markers = re.compile('markers=(.+?)\&')
@@ -18,6 +20,7 @@ def parse_imovel_page(data, context):
     '''
 
     try:
+        # Initializing the data
         error_client = error_reporting.Client()
         client = storage.Client()
         bucket = client.get_bucket('imoveis-data')
@@ -25,17 +28,21 @@ def parse_imovel_page(data, context):
         blob = bucket.blob(path_name)
         html_data = blob.download_as_string()
         soup = BeautifulSoup(html_data, 'lxml')
+        
         # Parsing the interesting data
         price_block = soup.select('div.block-price-container')
         attrs_block = soup.select('ul.section-icon-features')
         addts_block = soup.select('ul.section-bullets')
         local_block = soup.select('div.article-map')
-        scripts = soup.find_all('script')
+        
         pub_code = list(set(soup.find_all('span',{'class':'publisher-code'})))
         pub_date = soup.find('h5',{'class':['section-date','css-float-r']})
-        filter_scripts = list(filter(lambda val: regex_map.search(val.text), scripts))
-        # Transforming data indo a format of interest
 
+        scripts = soup.find_all('script')
+        filter_scripts = list(filter(lambda val: regex_map.search(val.text), scripts))
+        
+        # Transforming data indo a format of interest
+        # List of tuples
         final_tups = []
         description = soup.find('div', id='verDatosDescripcion')
         if description is not None:
@@ -87,11 +94,13 @@ def parse_imovel_page(data, context):
 
             if len(filter_scripts) > 0:
                 lat_long_script = filter_scripts[0].text
+                
                 lat_long = list(filter(lambda val: regex_map.search(val), lat_long_script.split('\n')))
                 lat_long = list(map(lambda val: tuple(
                     val.replace(' ', '').replace("'", '').replace(',', '').replace('mapLat', 'latitude').replace('mapLng',
                                                                                                                  'longitude').strip().split(
                         ':')), lat_long))
+                lat_long = [tuple((key,float(val))) for key,val in lat_long]
 
             else:
                 image_url = local_block[0].find('img')
@@ -99,7 +108,7 @@ def parse_imovel_page(data, context):
                 if regexp_markers.search(image_url):
                     url_parse = regexp_markers.search(image_url['src']).group(1).split(',')
                     lat_long = [float(float_val) for float_val in url_parse]
-                    lat_long = [('latitude', lat_long[0].replace(',', '')), ('longitute', lat_long[1].replace(',', ''))]
+                    lat_long = [('latitude', float(lat_long[0].replace(',', ''))), float('longitute', lat_long[1].replace(',', ''))]
 
             final_tups.extend(lat_long)
         
@@ -115,11 +124,15 @@ def parse_imovel_page(data, context):
         
         # Publication Date
         if pub_date is not None:
-            final_tups.append(('pub_data',pub_date))
+            final_tups.append(('pub_data',pub_date.text.strip()))
 
+        # No data gotten from value
         if len(final_tups) == 0:
             raise Exception('Impossible do parse %s'%path_name)
-            
+        
+        # Adding current date for bigquery
+        final_tups.append(('date',datetime.now()))
+
         json_file = json.dumps({unidecode.unidecode(key).strip().replace(' ', '_').lower(): val for key, val in final_tups})
         bucket = client.get_bucket('bigtable-data')
         new_blob = bucket.blob('{hex_name}.json'.format(hex_name=path_name.replace('.html', '')))
@@ -129,3 +142,4 @@ def parse_imovel_page(data, context):
     except Exception as error:
         error_client = error_reporting.Client()
         error_client.report_exception()
+
--- reload_html.py
+++ reload_html.py
@@ -1,20 +1,21 @@
+# Reloading the blobs into the pubsub
+
 from google.cloud import pubsub_v1,storage
 
 list_blobs = []
 string_split = '/b/imoveis-data/o/'
 
 
-topic = 'projects/educare-226818/topics/child_scrape'
-bucket_name = 'imoveis_data'
+topic = 'projects/educare-226818/topics/html_path'
+bucket_name = 'imoveis-data'
 storage_client = storage.Client()
 bucket = storage_client.get_bucket(bucket_name)
 
+# Getting the path of the blobs into a list
 for blob in bucket.list_blobs():
-    path = blob.path.split(bucket_name)[-1]
-    gs_path = 'gs://{bucket_name}/{path}'.format(bucket_name=bucket_name,path=path)
-    list_blobs.append(gs_path.encode('utf-8'))
+    path = ''.join(blob.media_link.split('/')[-1].split('?')[:-1]).encode('utf-8')
+    list_blobs.append((path))
 
 publisher = pubsub_v1.PublisherClient()
 for blob_path in list_blobs:
-    import pdb;pdb.set_trace()
     publisher.publish(topic=topic,data=blob_path)
