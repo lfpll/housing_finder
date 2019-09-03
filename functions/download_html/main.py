6f5a020a9fde2cb34c82179d851b3ac4f4f9bd32
Luiz Lobo <luizfpll@gmail.com>
2019-09-03 06:52:31 -0300

    adding blob exists and update folder

--- functions/download_html/main.py
+++ functions/download_html/main.py
@@ -20,7 +20,8 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0'}
 
 # Bucket where the data will be stored
-_IN_BUCKET = os.environ["DELIVER_BUCKET"]
+_OUT_BUCKET = os.environ["DELIVER_BUCKET"]
+_JSON_BUCKET = os.environ["JSON_BUCKET"]
 # 'projects/educare-226818/topics/child_scrape'
 _THIS_FUNCTION_TOPIC = os.environ["THIS_TOPIC"]
 # 'projects/educare-226818/topics/html_path'
@@ -46,51 +47,60 @@ def download_page(message, context):
         if tries < 5:
             publisher.publish(_THIS_FUNCTION_TOPIC, pub_obj_encoded)
         else:
-            logging.error(
-                "%s Was already parsed 5 times, ended with %s page", url, error)
-        quit()
+            raise Exception(
+                "%s was already parsed 5 times, ended with %s page"%(url, error))
     try:
-        # Getting the url to be paginated
-
+        # Getting the url to be paginated        
         data = base64.b64decode(message['data']).decode('utf-8')
         json_decoded = json.loads(data)
         url = json_decoded['url']
-        response = requests.get(url, headers=HEADERS)
-        publisher = pubsub_v1.PublisherClient()
+        
+        # Out file name to gsbucket
+        file_name = url.split('/')[-1]
 
-        # Adding number o tries
-        tries = 0
-        if 'tries' in json_decoded:
-            tries = int(json_decoded['tries']) + 1
-
-        # Object for failture
-        pub_obj_encoded = json.dumps({'url': url, 'tries': tries}).encode("utf-8")
-
-        # If the status is not 200 the requestor was blocked send back
-        if response.status_code != 200:
-            __error_path(publisher,pub_obj_encoded,tries,url,error=response.status_code)
+        storage_client = storage.Client()
+        # Checking if file exists, meaning it was an URL recreated
+        
+        bucket = storage_client.get_bucket(_OUT_BUCKET)
+        blob = bucket.blob(file_name)
+        if blob.exists():
+            bucket = storage_client.get_bucket(_OUT_BUCKET)
+            blob.upload_from_string('update/{0}'.format(file_name))
         else:
-            storage_client = storage.Client()
-            soup = BeautifulSoup(response.text, 'lxml')
+            response = requests.get(url, headers=HEADERS)
+            publisher = pubsub_v1.PublisherClient()
 
-            # Special case where this website bad implemented http errors
-            if soup.select('title')[0].text == 'Error 500':
-                __error_path(publisher,pub_obj_encoded,tries,url,error=500)
-                publisher.publish(_THIS_FUNCTION_TOPIC, url.encode('utf-8'))
+            # Adding number o tries
+            tries = 0
+            if 'tries' in json_decoded:
+                tries = int(json_decoded['tries']) + 1
+
+            # Object for failture
+            pub_obj_encoded = json.dumps({'url': url, 'tries': tries}).encode("utf-8")
+
+            # If the status is not 200 the requestor was blocked send back
+            if response.status_code != 200:
+                __error_path(publisher,pub_obj_encoded,tries,url,error=response.status_code)
             else:
-                # Saving the html by the url name
-                file_name = url.split('/')[-1]
-                pub_obj_encoded = json.dumps(
-                    {'file_path': file_name, 'url': url}).encode("utf-8")
+                
+                soup = BeautifulSoup(response.text, 'lxml')
 
-                # Opening the bucket connection
-                bucket = storage_client.get_bucket(_IN_BUCKET)
-                blob = bucket.blob(file_name)
-                blob.upload_from_string(response.content)
+                # Special case where this website bad implemented http errors
+                if soup.select('title')[0].text == 'Error 500':
+                    __error_path(publisher,pub_obj_encoded,tries,url,error=500)
+                    publisher.publish(_THIS_FUNCTION_TOPIC, url.encode('utf-8'))
+                else:
+                    # Saving the html by the url name
+                    
+                    pub_obj_encoded = json.dumps(
+                        {'file_path': file_name, 'url': url}).encode("utf-8")
 
-                # Publish path to be parsed and transformed to json
-                publisher.publish(_PARSE_FUNCTION_TOPIC, pub_obj_encoded)
+                    # Opening the bucket connection
+                    
+                    blob.upload_from_string(response.content)
 
+                    # Publish path to be parsed and transformed to json
+                    publisher.publish(_PARSE_FUNCTION_TOPIC, pub_obj_encoded)
     except Exception:
         error_client = error_reporting.Client()
         error_client.report_exception()
