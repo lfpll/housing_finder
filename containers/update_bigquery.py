51578265b4157ff6394b936bb177bf8143efc7e4
Luiz Lobo <luizfpll@gmail.com>
2019-09-29 20:35:00 -0300

    starting creating a class for removing bad urls

--- containers/update_bigquery.py
+++ containers/update_bigquery.py
@@ -1,44 +1,81 @@
 import os
 from google.cloud import bigquery
-import requests 
+import requests
 from bs4 import BeautifulSoup
 import time
-client = bigquery.Client()
-dataset = 'newdata'
-table = 'rentaldata'
 
-table_ref = client.dataset(dataset).table(table)
-table = client.get_table(table_ref)
-table_size = table.num_rows
 
-field_url = [bigquery.schema.SchemaField('url', 'STRING', 'NULLABLE', None, ())]
+def url_exists_imoveisweb(response):
+    """A function that receives a requests.response object and 
+      checks if the url is valid for the imoveis.web website
 
-list_urls = client.list_rows(table,selected_fields=field_url)
-
-temp_table = '''#standardSQL
-                CREATE TABLE  {dataset}.'temp_'{table}
-                OPTIONS(
-                expiration_timestamp=TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL 3 DAY)
-                ) AS
-                SELECT corpus, COUNT(*) c
-                FROM `bigquery-public-data.samples.shakespeare`
-                GROUP BY corpus'''
-
-def updated_list(delete_list):
-   query =  ''' UPDATE {dataset}.{table}
-                SET rental_deleted = 1
-                WHERE url in (SELECT * FROM {dataset}.'temp_'{table})
-
-            '''
-deleted_list = []
-for url in list_urls:
-    response = requests.get(url)
+    Args:
+        response ([type]): [description]
+    """
     if response.status_code != 200:
-        deleted_list.append(url)    
-    # If the request has error page 500 follow error path
+        return False
+    # Special case of bad status_code system
     soup = BeautifulSoup(response.content, 'lxml')
     if soup.select('title')[0].text == 'Error 500':
-        deleted_list.append(url)
-    if len(deleted_list) > 50:
-        
-    
+        return False
+    else:
+        return True
+
+
+class Check_Online_Urls:
+
+    def __init__(self, dataset='newdata', table='rentaldata',
+                 function_check_deleted=url_exists_imoveisweb, sleep_time=2):
+        # Function that checks if requests.response object was deleted
+        self.check_deleted = function_check_deleted
+        self.client = bigquery.Client()
+        self.dataset = dataset
+        self.table = table
+
+        self.sleep_time = sleep_time
+
+        # Passing parameterers to be less side effecty
+        self.urls_list = self.get_urls_bigquery(
+            dataset=self.dataset, table=self.table)
+
+        # Getting the urls that do not exist
+        self.deleted_urls = self.check_deleted(urls_list=self.urls_list,
+                                               wvalidation_function=self.check_deleted)
+
+    def get_urls_bigquery(self, dataset, table, url_columns='url'):
+        """Function that returns the urls from the column in bigquery
+
+        Args:
+            dataset ([type]): [description]
+            table ([type]): [description]
+            url_columns (str, optional): [description]. Defaults to 'url'.
+
+        Returns:
+            [type]: [description]
+        """
+        table_ref = self.client.dataset(dataset).table(table)
+        table = self.client.get_table(table_ref)
+        field_url = [bigquery.schema.SchemaField(
+            'url', 'STRING', 'NULLABLE', None, ())]
+        # Return the list of urls from the url colum
+        rows_list = self.client.list_rows(table, selected_fields=field_url)
+        return rows_list
+
+    def check_not_working_urls(self, urls_list, validation_function):
+        """A function that check urls offline based on a function
+
+        Args:
+            urls_list ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        delete_urls = []
+        for url in urls_list:
+            response = requests.get(url)
+            if validation_function(response):
+                delete_urls.append(url)
+        return delete_urls
+
+    def output_to_json(self, offline_list):
+        pass
