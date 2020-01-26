8d643f427623171e20a0c769a6dc7950d81438b0
Luiz Lobo <luizfpll@gmail.com>
2020-01-26 19:39:15 -0300

    updating bucket and blobs with metadata

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,14 @@ from os.path import dirname
 import requests
 import pytest
 from unittest.mock import MagicMock
-from mock_dbs import Mock_Sql_conn,Mock_Client_BigQuery, Mock_storage_client
-from google.cloud import pubsub_v1, logging, storage, bigquery
 from unittest.mock import patch
 from mock_gcloud import *
 PARENT_PATH = os.path.abspath(os.path.join(dirname(__file__), os.pardir))
 SAMPLES_FOLDER = PARENT_PATH + '/tests/samples/'
+TMP_FOLDER = PARENT_PATH + "/tmp/"
 sys.path.append(PARENT_PATH)
+
+os.environ["TMP_FOLDER"] = TMP_FOLDER
 
 @pytest.fixture()
 def current_folder():
@@ -19,5 +20,6 @@ def current_folder():
 @pytest.fixture()
 def sample_folder():
     return SAMPLES_FOLDER
+
 
 
