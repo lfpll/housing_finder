5c7aa4782a4acec036ea17407e7ae335ffad1daf
Luiz Lobo <luizfpll@gmail.com>
2020-01-26 12:24:12 -0300

    last updates on the jobs

--- jobs/daily_run.sh
+++ jobs/daily_run.sh
@@ -3,6 +3,7 @@ input_path="gs://imoveis-data-json/stage/*.json"
 output_path="gs://imoveis-data-json/out/"
 dataset="newdata"
 table="rentaldata"
+<<<<<<< HEAD
 schema_path=""
 
 
@@ -15,6 +16,18 @@ gcloud dataproc clusters delete dumdataproc --region us-central1
 
 # Loading the data into bigquery
 bq load --source_format PARQUET --noreplace --schena $schema_path  --autodetect $dataset.$table $output_path'*.parquet'
+=======
+
+
+# Treading the data with spark
+gcloud dataproc clusters create dumdataproc --region us-central1 --num-workers 2 --worker-machine-type custom-2-5120
+gcloud dataproc jobs submit pyspark --cluster dumdataproc schema_job.py $input_path
+gcloud dataproc jobs submit pyspark --cluster dumdataproc treating_imovelweb_data.py -- $input_path $output_path $dataset $table
+gcloud dataproc clusters delete dumdataproc --region us-central1
+
+# Loading the data
+bq load --source_format PARQUET --replace --autodetect $dataset.$table $output_path'*.parquet'
+>>>>>>> df946ad... last updates on the jobs
 
 # Moving data to backup
 gsutil mv gs://imoveis-data-json/stage gs://backup-json/$this_date
