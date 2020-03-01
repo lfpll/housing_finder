8a5a4fe9965f2bdca4bcebce16e30302f88439bd
Luiz Lobo <luizfpll@gmail.com>
2020-03-01 19:38:26 -0300

    job to ingest json data from gcs into sql

---
+++ treat_data/run_python_treatment.sh
@@ -0,0 +1,8 @@
+export TABLENAME="stage_data"
+export USER="sqlsvc"
+export IP='172.23.80.3'
+
+pythonPathFile=$1
+export PASSWORD=$2
+
+python3 $pythonPathFile
---
+++ treat_data/treat_bucket_to_sql.py
@@ -0,0 +1,84 @@
+# Imports the Google Cloud client library
+from google.cloud import storage
+import json
+import pandas as pd
+import re
+from numpy import nan
+import os
+from sqlalchemy import create_engine
+
+# Instantiates a client
+storage_client = storage.Client()
+
+# Return batch of json in a bucket subfolder
+def get_json_into_list(bucket_name:str ,subdir:str):
+    """Go into a subdirectory in bucket and returns a pack of json in a list
+    
+    Arguments:
+        bucket_name [str] -- [name of the bucket]
+        subdir [str] -- [name of the subdirectories]
+    
+    Returns:
+        [list(dict)] -- [list of dicts with the data of the jsons]
+    """    
+    bucket = storage_client.get_bucket(bucket_name)
+    blobs_list = bucket.list_blobs(prefix=subdir)
+    json_list = []
+    for blob in blobs_list:
+        json_list.append(json.loads(blob.download_as_string()))
+    return json_list
+
+def treat_imovelweb_data(imovelweb_df:pd.DataFrame):
+    """Specific class created to treate from the imovelweb site data
+    
+    Arguments:
+        imovelweb_df {pd.DataFrame} -- [data frame got from joining the json]
+    
+    Returns:
+        [pd.DataFrame] -- [returns the dataframe treated]
+    """    
+    tmp_df = imovelweb_df.copy()
+    regexp_non_words = re.compile(r'^\W+|\W+$',flags=re.UNICODE)
+    # Treating the publication date and transforming into an integer 
+    def replace_pub_data(prefix):
+        return prefix.lower().replace('hoje','0').replace('ontem','1')
+
+    # Remove spaces without afecting accent words
+    def strip_with_utf8(string):
+        if string is nan:
+            return nan
+        return regexp_non_words.sub('',string)
+
+    #  Joining columns that have the same meaning
+    tmp_df["banheiros"] = tmp_df[["banheiros","banheiro"]].bfill(axis=1).iloc[:, 0]
+    tmp_df["vagas"] = tmp_df[["vagas","vaga"]].bfill(axis=1).iloc[:, 0]
+    tmp_df["suites"] = tmp_df[["suites","suite"]].bfill(axis=1).iloc[:, 0]
+    tmp_df["quartos"] = tmp_df[["quartos","quarto"]].bfill(axis=1).iloc[:, 0]
+    tmp_df.drop(columns=["banheiro","vaga","suite","quarto"],inplace=True)
+
+    # Correcting the creation date
+    tmp_df['pub_data'] = tmp_df['pub_data'].apply(replace_pub_data).str.replace("[^0-9]","")
+
+    # Removing "m2"
+    tmp_df['area_util'] = tmp_df['area_util'].str.replace('m2','').astype("Float32")
+    tmp_df['area_total'] = tmp_df['area_total'].str.replace('m2','').astype("Float32")
+
+    # Removing spaces or bizarre values at the start or beggining of the string columns
+    string_columns= tmp_df.select_dtypes('object').drop(columns=['additions','imgs'])
+    tmp_df[string_columns.columns.values] = string_columns.applymap(strip_with_utf8)
+
+
+    tmp_df[['bairro','cidade']] = tmp_df['bairro'].str.split(",",expand=True)
+    return tmp_df
+
+
+if __name__ == "__main__":
+    USER = os.environ["USER"]
+    PWD = os.environ["PASSWORD"]
+    IP = os.environ["IP"]
+    TABLE_NAME = os.environ['TABLE_NAME']
+    json_list = get_json_into_list("imoveis-data-bigtable","stage")
+    treated_df = treat_imovelweb_data(pd.DataFrame(json_list))
+    db_string = "postgres://{user}:{password}@{ip}/postgres".format(user=USER,password=PWD,ip=IP)
+    db_conn = create_engine(db_string)
+    treated_df.to_sql(TABLE_NAME,db_conn)
