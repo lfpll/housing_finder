459c6cc92e2f0ac1fe6d9d8d8310e3fab0a3ab38
Luiz Lobo <luizfpll@gmail.com>
2020-04-19 15:14:01 -0300

    renaming files of shell scripts

--- data_mainetance/ingest_data_and_delete.sh
+++ data_mainetance/ingest_data_and_delete.sh
@@ -1,11 +1,12 @@
 export USER="postgres"
 export IP="0.0.0.0"
 export TABLE_NAME="imoveis_online"
-export OFFLINE_TABLE_NAME="imoveis_offline"
+export STAGE_TABLE_NAME="imoveis_stage"
 export SERVER="postgres"
 export DATABASE="postgres"
-export PASSWORD="luiz28"
-
+export LOG_LEVEL="INFO"
 
 python3 ./treat_bucket_to_sql.py
 python3 ./remove_offline_urls_from_sql.py
+
+sudo shutdown
