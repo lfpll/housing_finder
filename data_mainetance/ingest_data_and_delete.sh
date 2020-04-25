c299ba686508475de44fe8b8ad4e9283ac68a902
Luiz Lobo <luizfpll@gmail.com>
2020-04-25 11:46:53 -0300

    adding backup of tables to gcs

--- data_mainetance/ingest_data_and_delete.sh
+++ data_mainetance/ingest_data_and_delete.sh
@@ -2,11 +2,12 @@ export USER="postgres"
 export IP="0.0.0.0"
 export TABLE_NAME="imoveis_online"
 export STAGE_TABLE_NAME="imoveis_stage"
+export OFFLINE_TABLE_NAME="imoveis_offline"
 export SERVER="postgres"
 export DATABASE="postgres"
 export LOG_LEVEL="INFO"
 
 python3 ./treat_bucket_to_sql.py
 python3 ./remove_offline_urls_from_sql.py
-gsutil rm gs://imoveis-data-bigtable/stage/*
+python3 ./backup_to_gcs
 sudo shutdown
